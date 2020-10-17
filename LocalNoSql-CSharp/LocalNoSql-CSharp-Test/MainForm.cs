2ac5154dd636a23174ca6687ffc4c0f729ad8258
Predoni Traian <predoni@gmail.com>
2020-10-17 13:56:25 +0300

    The first projects have been added.

---
+++ LocalNoSql-CSharp/LocalNoSql-CSharp-Test/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocalNoSql_CSharp_Test
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            LocalNoSql_CSharp.TestLib.ExecuteTest();
+        }
+    }
+}
