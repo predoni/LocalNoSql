6e1c81b2fc61dd688d8326dfc0ee33a95f9e2b65
Predoni Traian <predoni@gmail.com>
2020-10-18 12:44:04 +0300

    I had finished the basic implementation and testings for class Database. This class will be modified in the future.

--- LocalNoSql-CSharp/LocalNoSql-CSharp-NUnitTest/DB/Nu_Database.cs
+++ LocalNoSql-CSharp/LocalNoSql-CSharp-NUnitTest/DB/Nu_Database.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Reflection;
 
 namespace LocalNoSql_CSharp_NUnitTest.DB
 {
@@ -10,6 +11,7 @@ namespace LocalNoSql_CSharp_NUnitTest.DB
         {
         }
 
+        [Test, Order(1)] // You can see the order in debug. Is correct!
         [TestCase("C:\\testdb", "")]
         [TestCase("", "testDBName")]
         [TestCase("C:\\testdb\\", "testDBNa????me")]
@@ -19,19 +21,18 @@ namespace LocalNoSql_CSharp_NUnitTest.DB
         [TestCase("C:\\testdb\\", "testDBName")]
         public void Constructor(string rootPath, string databaseName)
         {
+            System.Diagnostics.Debug.WriteLine("Start testing: {0}", new[] { System.Reflection.MethodInfo.GetCurrentMethod().Name });
+
             try
             {
                 LocalNoSql_CSharp.DB.Database database = new LocalNoSql_CSharp.DB.Database(rootPath, databaseName);
 
                 Assert.Pass
                 (
-                    string.Format
-                    (
-                        "{0}: {1}{2}{3}: {4}{5}{6}: {7}",
-                        nameof(database.RootPath), database.RootPath, Environment.NewLine,
-                        nameof(database.DatabaseName), database.DatabaseName, Environment.NewLine,
-                        nameof(database.FullDatabasePath), database.FullDatabasePath
-                    )
+                    "{0}: {1}{2}{3}: {4}{5}{6}: {7}", 
+                    nameof(database.RootPath), database.RootPath, Environment.NewLine,
+                    nameof(database.DatabaseName), database.DatabaseName, Environment.NewLine,
+                    nameof(database.FullDatabasePath), database.FullDatabasePath
                 );
             }
             catch (NUnit.Framework.SuccessException e)
@@ -40,20 +41,29 @@ namespace LocalNoSql_CSharp_NUnitTest.DB
             }
             catch (Exception e)
             {
-                System.Diagnostics.Debug.WriteLine(string.Format("{0}: {1}", nameof(rootPath), rootPath));
-                System.Diagnostics.Debug.WriteLine(string.Format("{0}: {1}", nameof(databaseName), databaseName));
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", nameof(rootPath), rootPath);
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", nameof(databaseName), databaseName);
                 System.Diagnostics.Debug.WriteLine(e.Message + Environment.NewLine);
             }
         }
 
+        [Test, Order(2)] // You can see the order in debug. Is correct!
         [TestCase("C:\\testdb\\", "testDBName")]
         [TestCase("C:\\testdb\\", "ExistentDB")]
         public void Exists(string rootPath, string databaseName)
         {
+            System.Diagnostics.Debug.WriteLine("Start testing: {0}", new[] { System.Reflection.MethodInfo.GetCurrentMethod().Name });
+
             try
             {
                 LocalNoSql_CSharp.DB.Database database = new LocalNoSql_CSharp.DB.Database(rootPath, databaseName);
-                Assert.Pass(string.Format("{0}: {1} => exists: {2}", nameof(database.FullDatabasePath), database.FullDatabasePath, database.Exists().ToString()));
+                Assert.Pass(
+                    "{0}: {1} => {2}: {3}", 
+                    nameof(database.FullDatabasePath), 
+                    database.FullDatabasePath, 
+                    nameof(database.Exists), 
+                    database.Exists().ToString()
+                );
             }
             catch (NUnit.Framework.SuccessException e)
             {
@@ -61,8 +71,68 @@ namespace LocalNoSql_CSharp_NUnitTest.DB
             }
             catch (Exception e)
             {
-                System.Diagnostics.Debug.WriteLine(string.Format("{0}: {1}", nameof(rootPath), rootPath));
-                System.Diagnostics.Debug.WriteLine(string.Format("{0}: {1}", nameof(databaseName), databaseName));
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", nameof(rootPath), rootPath);
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", nameof(databaseName), databaseName);
+                System.Diagnostics.Debug.WriteLine(e.Message + Environment.NewLine);
+            }
+        }
+
+        [Test, Order(3)] // You can see the order in debug. Is correct!
+        [TestCase("C:\\testdb\\", "newDBToBeCreated")]
+        [TestCase("C:\\testdb\\", "ExistentDB")]
+        public void Create(string rootPath, string databaseName)
+        {
+            System.Diagnostics.Debug.WriteLine("Start testing: {0}", new[] { System.Reflection.MethodInfo.GetCurrentMethod().Name });
+
+            try
+            {
+                LocalNoSql_CSharp.DB.Database database = new LocalNoSql_CSharp.DB.Database(rootPath, databaseName);
+                Assert.Pass(
+                    "{0}: {1} => {2}: {3}",
+                    nameof(database.FullDatabasePath),
+                    database.FullDatabasePath,
+                    nameof(database.Create),
+                    database.Create().ToString()
+                );
+            }
+            catch (NUnit.Framework.SuccessException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message + Environment.NewLine);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", nameof(rootPath), rootPath);
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", nameof(databaseName), databaseName);
+                System.Diagnostics.Debug.WriteLine(e.Message + Environment.NewLine);
+            }
+        }
+
+        [Test, Order(4)] // You can see the order in debug. Is correct!
+        [TestCase("C:\\testdb\\", "newDBToBeCreated")]
+        [TestCase("C:\\testdb\\", "NonExistentDB")]
+        public void Delete(string rootPath, string databaseName)
+        {
+            System.Diagnostics.Debug.WriteLine("Start testing: {0}", new[] { System.Reflection.MethodInfo.GetCurrentMethod().Name });
+
+            try
+            {
+                LocalNoSql_CSharp.DB.Database database = new LocalNoSql_CSharp.DB.Database(rootPath, databaseName);
+                Assert.Pass(
+                    "{0}: {1} => {2}: {3}",
+                    nameof(database.FullDatabasePath),
+                    database.FullDatabasePath,
+                    nameof(database.Delete),
+                    database.Delete().ToString()
+                );
+            }
+            catch (NUnit.Framework.SuccessException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message + Environment.NewLine);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", nameof(rootPath), rootPath);
+                System.Diagnostics.Debug.WriteLine("{0}: {1}", nameof(databaseName), databaseName);
                 System.Diagnostics.Debug.WriteLine(e.Message + Environment.NewLine);
             }
         }
--- LocalNoSql-CSharp/LocalNoSql-CSharp/DB/Database.cs
+++ LocalNoSql-CSharp/LocalNoSql-CSharp/DB/Database.cs
@@ -86,19 +86,66 @@ namespace LocalNoSql_CSharp.DB
         /// <summary>
         /// Creates the database if it does not exists.
         /// </summary>
+        /// <exception cref="IOException">
+        /// The directory specified by path is a file.
+        /// -or-
+        /// The network name is not known.
+        /// </exception>
+        /// <exception cref="UnauthorizedAccessException">The caller does not have the required permission.</exception>
+        /// <exception cref="ArgumentException">
+        /// path is a zero-length string, contains only white space, or contains one or more invalid characters.You can query for invalid characters by using the GetInvalidPathChars() method.
+        /// -or-
+        /// path is prefixed with, or contains, only a colon character(:).
+        /// </exception>
+        /// <exception cref="ArgumentNullException">path is null.</exception>
+        /// <exception cref="PathTooLongException">The specified path, file name, or both exceed the system-defined maximum length.</exception>
+        /// <exception cref="DirectoryNotFoundException">The specified path is invalid(for example, it is on an unmapped drive).</exception>
+        /// <exception cref="NotSupportedException">path contains a colon character(:) that is not part of a drive label("C:\").</exception>
         /// <returns>Returns true if the database was created successfully or if already exists, otherwise false.</returns>
         public bool Create()
         {
-            throw new NotImplementedException();
+            if (this.Exists())
+                // Is already created.
+                return true;
+
+            // Discard the returned value.
+            _ = System.IO.Directory.CreateDirectory(this.FullDatabasePath);
+
+            return true;
         }
 
         /// <summary>
-        /// Delete a database.
+        /// Delete a database directory with its content.
         /// </summary>
+        /// <exception cref="IOException">
+        /// A file with the same name and location specified by path exists.
+        /// -or-
+        /// The directory is the application's current working directory.
+        /// -or-
+        /// The directory specified by path is not empty.
+        /// -or-
+        /// The directory is read-only or contains a read-only file.
+        /// -or-
+        /// The directory is being used by another process.
+        /// </exception>
+        /// <exception cref="UnauthorizedAccessException">The caller does not have the required permission.</exception>
+        /// <exception cref="ArgumentException">path is a zero-length string, contains only white space, or contains one or more invalid characters.You can query for invalid characters by using the GetInvalidPathChars() method.</exception>
+        /// <exception cref="ArgumentNullException">path is null.</exception>
+        /// <exception cref="PathTooLongException">The specified path, file name, or both exceed the system-defined maximum length.</exception>
+        /// <exception cref="DirectoryNotFoundException">
+        /// path does not exist or could not be found.
+        /// -or-
+        /// The specified path is invalid(for example, it is on an unmapped drive).</exception>
         /// <returns>Returns true on success, otherwise false.</returns>
         public bool Delete()
         {
-            throw new NotImplementedException();
+            if (!this.Exists())
+                // Has already been deleted.
+                return true;
+
+            System.IO.Directory.Delete(this.FullDatabasePath, true);
+
+            return true;
         }
         #endregion
     }
